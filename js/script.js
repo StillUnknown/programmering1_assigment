3c49a4c71cc13a7a48e786e57d6199047e52be93
StillUnknown <knavinglogistic@hotmail.se>
2021-10-21 12:02:18 +0200

    pushar inlämningen

---
+++ js/script.js
@@ -0,0 +1,83 @@
+function openShoppingList() {
+    document.getElementById('shoppingcart-dropdown')
+        .classList.toggle('show')
+}
+
+function closeShoppingList() {
+    document.getElementById('shoppingcart-dropdown')
+        .classList.remove('show')
+}
+
+const hardstyleOneCdPrice = 229
+function priceOfHardstyleOne() {
+    document.getElementById('hardstyleOnePrice').innerText = hardstyleOneCdPrice
+}
+priceOfHardstyleOne()
+
+const hardstyleTwoCdPrice = 259
+function priceOfHardstyleTwo() {
+    document.getElementById('hardstyleTwoPrice').innerText = hardstyleTwoCdPrice
+}
+priceOfHardstyleTwo()
+
+const hardstyleThreeCdPrice = 119
+function priceOfHardstyleThree() {
+    document.getElementById('hardstyleThreePrice').innerText = hardstyleThreeCdPrice
+}
+priceOfHardstyleThree()
+
+const hardstyleFourCdPrice = 239
+function priceOfHardstyleFour() {
+    document.getElementById('hardstyleFourPrice').innerText = hardstyleFourCdPrice
+}
+priceOfHardstyleFour()
+
+const hardstyleFiveCdPrice = 239
+function priceOfHardstyleFive() {
+    document.getElementById('hardstyleFivePrice').innerText = hardstyleFiveCdPrice
+}
+priceOfHardstyleFive()
+
+const hardstyleSixCdPrice = 259
+function priceOfHardstyleSix() {
+    document.getElementById('hardstyleSixPrice').innerText = hardstyleSixCdPrice
+}
+priceOfHardstyleSix()
+
+const hardstyleSevenCdPrice = 259
+function priceOfHardstyleSeven() {
+    document.getElementById('hardstyleSevenPrice').innerText = hardstyleSevenCdPrice
+}
+priceOfHardstyleSeven()
+
+const hellstromCdPrice = 99
+function priceOfHellstromCd() {
+    document.getElementById('hellstromPrice').innerText = hellstromCdPrice
+}
+priceOfHellstromCd()
+
+function justForLaugh() {
+    alert('We dont really sell this CD, its just here for the laughs')
+}
+
+function removeButton() {
+    alert('Your item was removed from the shoppingcart')
+}
+
+const costOfFreeShipping = 350
+function leftForFreeShipping() {
+    document.getElementById('left-to-free-shipping').innerText = costOfFreeShipping
+}
+leftForFreeShipping()
+
+function purchaseAlert() {
+    alert('Your face was successfully scanned and your purchase will be credited!')
+}
+
+function hardstyleOneBuyButton() {
+    document.getElementById('shoppingcart-dropdown').innerText = hardstyleOneCdPrice
+}
+
+function hardstyleTwoBuyButton() {
+    document.getElementById('shoppingcart-dropdown').innerText = hardstyleTwoCdPrice
+}
