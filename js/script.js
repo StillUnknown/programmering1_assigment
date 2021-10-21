dad3e5fc88f28a6b994ac4ae8b766f5e5dd60f18
StillUnknown <knavinglogistic@hotmail.se>
2021-10-21 12:05:04 +0200

    pushar inlämningen

--- js/script.js
+++ js/script.js
@@ -81,3 +81,7 @@ function hardstyleOneBuyButton() {
 function hardstyleTwoBuyButton() {
     document.getElementById('shoppingcart-dropdown').innerText = hardstyleTwoCdPrice
 }
+
+function hardstyleThreeBuyButton() {
+    
+}
