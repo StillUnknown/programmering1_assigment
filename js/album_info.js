879801a4cbcf8036c4b4eb86e3d8d3b922e4e359
StillUnknown <knavinglogistic@hotmail.se>
2021-10-24 18:48:35 +0200

    Ändrar från 8 func till en array med 8 object istället

--- js/album_info.js
+++ js/album_info.js
@@ -1,3 +1,78 @@
+let albums = [
+    {
+        image: "img/hardstyle-one.PNG",
+        name: "Mixed Artists - Hardstyle 2017",
+        type: "CD",
+        price: 229
+    },
+    {
+        image: "img/hardstyle-two.PNG",
+        name: "Mixed Artists - Hardstyle 2018",
+        type: "CD",
+        price: 259
+    },
+    {
+        image: "img/hardstyle-three.PNG",
+        name: "Mixed Artists - World Of Hardstyle",
+        type: "CD",
+        price: 119
+    },
+    {
+        image: "img/hardstyle-four.PNG",
+        name: "Mixed Artists - Ultimate Hardstyle",
+        type: "CD",
+        price: 239
+    },
+    {
+        image: "img/hardstyle-five.PNG",
+        name: "Mixed Artists - Hardstyle 2020",
+        type: "CD",
+        price: 219
+    },
+    {
+        image: "img/hardstyle-six.PNG",
+        name: "Mixed Artists - Hardstyle 2021",
+        type: "CD",
+        price: 289
+    },
+    {
+        image: "img/hardstyle-seven.PNG",
+        name: "Mixed Artists - Hardstyle 2022",
+        type: "CD",
+        price: 277
+    },
+    {
+        image: "img/HåkanHellström.PNG",
+        name: "Håkan Hellström - Illusioner",
+        type: "CD",
+        price: 99
+    },
+    ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function albumOneContainer() {
     return `
 <section class="album-one">
