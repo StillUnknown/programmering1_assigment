6f14a729ffb173863d8881e247a91e45b4fcf410
StillUnknown <knavinglogistic@hotmail.se>
2021-10-27 19:23:23 +0200

    2021-10-27 more fix

--- js/albums_in_array.js
+++ js/albums_in_array.js
@@ -57,34 +57,7 @@ let albums = [
     },
 ]
 
-function renderSingleAlbum(album) {
-    return `
-    <article id=albumToBy>
-        <img src=${album.image} alt=${album} class="img" id=${album}>
-            <p>Title: ${album.name}</p>
-            <p>Price: ${album.price}:-</p>
-            <button id="${album.id}">Buy</button>
-    </article> `
-        ;
-}
 
-let myAlbumCards = [];
-
-function updateElementIdHtml(elementId, html) {
-    document.getElementById(elementId).innerHTML = html
-}
-
-function updateContent() {
-    let htmlElement = ''
-
-    for (const album of albums) {
-        htmlElement += renderSingleAlbum(album)
-    }
-
-    updateElementIdHtml('album-grid-container', htmlElement)
-}
-
-updateContent()
 
 function itemAlreadyInCart(id) {
     for (const id of myAlbumCards) {
